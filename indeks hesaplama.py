dfd9d8eec7484e6320ecfe124fcfa28407dff2c7
EasyTricks-Scripts <caganprogamer11@gmail.com>
2025-08-11 17:49:44 +0300

    Add files via upload
    index calculator

---
+++ indeks hesaplama.py
@@ -0,0 +1,39 @@
+print("Adınız: ")
+ad=input()
+
+
+def index():
+    global boy
+    global yas
+    global kilo
+
+print("Yaş kaç bro: ")
+yas=int(input())
+
+print("Güzel....")
+print("Boyunuz kaç")
+
+boy=int(input())
+
+print("GÜZEL")
+print("Son olarak kilonuz kaç")
+
+kilo=int(input())
+
+print ("Güzelllll")
+
+index= (kilo / boy*boy)
+
+print("İndexsiniz ", index)
+
+if index==50:
+    print("Normal")
+elif index > 50:
+    print("Kilolu")
+else:
+    print("Zayıf")
+
+
+
+
+    
